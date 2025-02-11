e384b58ce09b18dc30ab712ad6469e13279561a3
xxh296 <140650758+xxh296@users.noreply.github.com>
2025-02-11 11:34:18 -0800

    Add `checkForGameover()` * Added the function to check, if there is a winner. * The check is done after every move. * `checkForGameover()` returns "X", "O", or "It's a tie!" (if no empty cells are left). * Returns `null`, if there is no winner yet, and there are empty cells.

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ hello.textContent += "Hello from script.js";
 
 const play = (function () {
     const Gameboard = {
-        CELL_1: "", CELL_2: "", CELL_3: "",
-        CELL_4: "", CELL_5: "", CELL_6: "",
-        CELL_7: "", CELL_8: "", CELL_9: "",
+        A_1: "", B_1: "", C_1: "",
+        A_2: "", B_2: "", C_2: "",
+        A_3: "", B_3: "", C_3: "",
     }
 
     // mark an empty cell as X
     const makeMoveX = (cell) => {
         if (Gameboard[cell] === ""){
             Gameboard[cell] = "X";
+            return checkForGameover();
         } else {
             console.log("This cell is already used.")
         }
@@ -22,6 +23,7 @@ const play = (function () {
     const makeMoveO = (cell) => {
         if (Gameboard[cell] === ""){
             Gameboard[cell] = "O";
+            return checkForGameover();
         } else {
             console.log("This cell is already used.")
         }
@@ -37,10 +39,130 @@ const play = (function () {
 
 // DEBUG / tests
 
-play.makeMoveX("CELL_1");
-console.log(play.Gameboard); // print the Gameboard
-console.log(play.Gameboard.CELL_1); // individual cells accessible? 
+// play.makeMoveX("A_1");
+// console.log(play.Gameboard); // print the Gameboard
+// console.log(play.Gameboard.A_1); // individual cells accessible? 
 
-play.makeMoveX("CELL_1"); // testing used cell
-play.makeMoveO("CELL_2"); // test an O cell
-console.log(play.Gameboard); // print the Gameboard
+// play.makeMoveX("A_1"); // testing used cell
+// play.makeMoveO("B_1"); // test an O cell
+// console.log(play.Gameboard); // print the Gameboard
+
+/*
+
+play.makeMoveO("A_1");
+console.log(checkForGameover()); // undefined
+play.makeMoveO("A_2");
+console.log(checkForGameover()); // undefined
+play.makeMoveO("A_3");
+console.log(checkForGameover());
+
+*/
+
+
+
+
+
+
+// check for gameover (refactored for switch, added the 'tie')
+function checkForGameover() {
+    const winningLines = [
+        ["A_1", "A_2", "A_3"],
+        ["B_1", "B_2", "B_3"],
+        ["C_1", "C_2", "C_3"],
+        ["A_1", "B_1", "C_1"],
+        ["A_2", "B_2", "C_2"],
+        ["A_3", "B_3", "C_3"],
+        ["A_1", "B_2", "C_3"],
+        ["A_3", "B_2", "C_1"]
+    ];
+
+    for (const [cell1, cell2, cell3] of winningLines) {
+        const value = play.Gameboard[cell1];
+
+        if (value && value === play.Gameboard[cell2] && value === play.Gameboard[cell3]) {
+            switch (value) {
+                case "X":
+                    return "X"; // X wins
+                case "O":
+                    return "O"; // O wins
+            }
+        }
+    }
+
+    // Check if there are any empty spaces left
+    const isTie = !Object.values(play.Gameboard).includes("");
+    if (isTie) {
+        return "It's a tie!";
+    }
+
+    return null; // No winner yet
+};
+
+// // check for gameover
+// function checkForGameover(){
+//     if ((play.Gameboard.A_1 === play.Gameboard.A_2 && play.Gameboard.A_2 === play.Gameboard.A_3)){
+//         if (play.Gameboard.A_1 === "X"){
+//             return "X";
+//         } else if (play.Gameboard.A_1 === "O"){
+//             return "O";
+//         }
+//     }
+
+//     if ((play.Gameboard.B_1 === play.Gameboard.B_2 && play.Gameboard.B_2 === play.Gameboard.B_3)){
+//         if (play.Gameboard.B_1 === "X"){
+//             return "X";
+//         } else if (play.Gameboard.B_1 === "O"){
+//             return "O";
+//         }
+//     }
+
+//     if ((play.Gameboard.C_1 === play.Gameboard.C_2 && play.Gameboard.C_2 === play.Gameboard.C_3)){
+//         if (play.Gameboard.C_1 === "X"){
+//             return "X";
+//         } else if (play.Gameboard.C_1 === "O"){
+//             return "O";
+//         }
+//     }
+
+//     if ((play.Gameboard.A_1 === play.Gameboard.B_1 && play.Gameboard.B_1 === play.Gameboard.C_1)){
+//         if (play.Gameboard.A_1 === "X"){
+//             return "X";
+//         } else if (play.Gameboard.A_1 === "O"){
+//             return "O";
+//         }
+//     }
+
+//     if ((play.Gameboard.A_2 === play.Gameboard.B_2 && play.Gameboard.B_2 === play.Gameboard.C_2)){
+//         if (play.Gameboard.A_2 === "X"){
+//             return "X";
+//         } else if (play.Gameboard.A_2 === "O"){
+//             return "O";
+//         }
+//     }
+
+//     if ((play.Gameboard.A_3 === play.Gameboard.B_3 && play.Gameboard.B_3 === play.Gameboard.C_3)){
+//         if (play.Gameboard.A_3 === "X"){
+//             return "X";
+//         } else if (play.Gameboard.A_3 === "O"){
+//             return "O";
+//         }
+//     }
+
+//     if ((play.Gameboard.A_1 === play.Gameboard.B_2 && play.Gameboard.B_2 === play.Gameboard.C_3)){
+//         if (play.Gameboard.A_1 === "X"){
+//             return "X";
+//         } else if (play.Gameboard.A_1 === "O"){
+//             return "O";
+//         }
+//     }
+
+//     if ((play.Gameboard.A_3 === play.Gameboard.B_2 && play.Gameboard.B_2 === play.Gameboard.C_1)){
+//         if (play.Gameboard.A_3 === "X"){
+//             return "X";
+//         } else if (play.Gameboard.A_3 === "O"){
+//             return "O";
+//         }
+//     }
+// }
+
+
